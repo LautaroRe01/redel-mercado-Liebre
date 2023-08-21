1659b6a53b67b05723d1f41d5668defd34eb0855
LautaroRe01 <lautaroigredel@gmail.com>
2023-08-21 18:27:56 -0300

    adds styles

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 const PORT = 3001;
-
-app.listen(PORT,  () => {
-    console.log(`Se prendio en el puerto ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Se prendió en el puerto ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"));
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
 });
